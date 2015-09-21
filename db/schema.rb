ddd521022ad71d6c7db9b43b1ab69f84ee9b8def
apalga <art.palnor@gmail.com>
2015-09-21 01:07:26 +0000

    se realizo un cambio

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20150920061811) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",     limit: 255
     t.string   "last_sign_in_ip",        limit: 255
+    t.string   "name",                   limit: 255
+    t.string   "permission_level",       limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
   end
