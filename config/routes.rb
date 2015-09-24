1d765556b1ada5f4b7f430330080eb9cfcd97054
juanjoseguibohiga <juanjotoshi2@gmail.com>
2015-09-24 03:23:36 +0000

    Se añadieron nuevas cosas

--- config/routes.rb
+++ config/routes.rb
@@ -55,7 +55,7 @@ Rails.application.routes.draw do
 
   #resources :users
   #get 'home/index'
-  root 'home#index'
+  root 'static#inicio'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
